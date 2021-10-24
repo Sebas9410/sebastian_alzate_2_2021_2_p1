cacf1f6a24948faef66f1aadca9d95b5f34ec8e5
Sebas9410 <53840547+Sebas9410@users.noreply.github.com>
2021-10-24 09:51:37 -0500

    connection chain

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:elephants/helpers/constans.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,9 +14,30 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   @override
+  void initState() {
+    super.initState();
+    _getElephants();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login Screen')),
+      body: Center(
+          child: Column(
+        children: <Widget>[],
+      )),
     );
   }
+}
+
+void _getElephants() async {
+  var url = Uri.parse('${Constans.apiUrl}');
+  var response = await http.post(
+    url,
+    headers: {
+      'conten-type': 'aplication/json',
+      'accept': 'aplication/json',
+    },
+  );
+  print(response);
 }
